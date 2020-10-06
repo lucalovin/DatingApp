229679054853643b870d7b41ab5984371258e25b
Luca Lovin <dan_lovin@trimble.com>
2020-10-06 11:45:11 +0300

    End of section 6

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Toast, ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
