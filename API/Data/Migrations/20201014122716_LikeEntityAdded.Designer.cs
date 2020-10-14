4a60203671334133aedc349c2f8e9a7714c529cf
Luca Lovin <dan_lovin@trimble.com>
2020-10-14 18:42:05 +0300

    End of section 14

--- API/Data/Migrations/20201014122716_LikeEntityAdded.Designer.cs
+++ API/Data/Migrations/20201014122716_LikeEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201014122716_LikeEntityAdded")]
+    partial class LikeEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -60,7 +62,7 @@ namespace API.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Users");
+                    b.ToTable("AppUser");
                 });
 
             modelBuilder.Entity("API.Entities.Photo", b =>
@@ -100,7 +102,7 @@ namespace API.Data.Migrations
 
                     b.HasIndex("LikedUserId");
 
-                    b.ToTable("Likes");
+                    b.ToTable("UserLike");
                 });
 
             modelBuilder.Entity("API.Entities.Photo", b =>
